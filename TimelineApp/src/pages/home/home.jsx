40544018850ec987394b39d06ed0358c85e24d40
Allan Elias <111256927+EliasAllan@users.noreply.github.com>
2023-12-05 02:46:06 -0500

    started homepage and added styling

---
+++ TimelineApp/src/pages/home/home.jsx
@@ -0,0 +1,45 @@
+import './timeline.css'
+import moments from '../../assets/momentData.json'
+function Home() {
+  
+    return (
+      <>
+       <link rel="stylesheet" href="timeline.css"></link>
+        <h3 class="hints">
+      Click on given circles to see more info
+    </h3>
+
+    {/* {{#if logged_in}}
+<button onclick="window.location.href='/newmoment'" id="add-moment-btn">Add a moment in time</button>
+    {{/if}} */}
+    
+    <button id="get-history">What else happened on this day ?</button>
+    <p id="historical">
+    </p>
+
+    <div class="timeline-wrapper">
+      <ul class="timeline">
+        {/* {{#each moments as |moment| }}
+        <li data-date="{{moment.date}}">
+          <span class="title">{{moment.title}}</span>
+          <div id="moment-dots" class="data show">
+            <h3 class="description">{{moment.description}}</h3>
+            <small class="date"> {{moment.date}}</small>
+            <p class="^details">
+              {{moment.details}}
+            </p>
+            <button data-id={{moment.id}}>What else happened on this day ?</button>
+
+
+            {{!-- <span class="close">Click to close</span> --}}
+          </div>
+        </li>
+        {{/each}} */}
+    
+      </ul>
+    </div>
+      </>
+    )
+  }
+  
+  export default Home
