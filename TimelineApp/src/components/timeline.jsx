64bec948376117df8dfe90de6e6a728b74493d98
Allan Elias <111256927+EliasAllan@users.noreply.github.com>
2024-05-27 18:32:03 -0400

    hoizontal line

--- TimelineApp/src/components/timeline.jsx
+++ TimelineApp/src/components/timeline.jsx
@@ -1,29 +1,232 @@
-import react from "react"
-import { Chrono } from "react-chrono"
+import react from "react";
+import { Chrono } from "react-chrono";
 
 const Timeline = () => {
-    const moments = [{
-        // date:"2023-07-24",
-        // title: "Powering through development.",
-        // cardTitle: "In need of some inspiration, or even better, discipline.",
-        // details: "Late night coding session , 3 days till BIG DUB (the local rave). Hopefully that'll give me the inspiration I need.",
-        // user_id: "2"
-        title: "May 1940",
+  // const items = [{
+  //     title: "May 1940",
+  //     cardTitle: "Dunkirk",
+  //     url: "http://www.history.com",
+  //     cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+  //     cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+  //     media: {
+  //       type: "IMAGE",
+  //       source: {
+  //         url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif"
+  //       }
+  //     }
+  //   }];
+
+  //   return (
+  //     <div style={{ width: "500px", height: "400px" }}>
+  //       <Chrono items={items} />
+  //     </div>
+  //   )
+  const items = [
+    {
+      // date:"2023-07-24",
+      // title: "Powering through development.",
+      // cardTitle: "In need of some inspiration, or even better, discipline.",
+      // details: "Late night coding session , 3 days till BIG DUB (the local rave). Hopefully that'll give me the inspiration I need.",
+      // user_id: "2"
+      title: "May 1940",
       cardTitle: "Dunkirk",
       url: "http://www.history.com",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
       media: {
         type: "IMAGE",
         source: {
-          url: "https://drive.google.com/file/d/0B_566pSbggqLLVBzQ3ZSUHBKRDg/view?usp=drive_link&resourcekey=0-HXU6FMKOb-HTKfyA5JA_XA"
-        }
-      }
-    }]
-    return (
-        <div style={{ width: "500px", height: "400px" }}>
-        <Chrono moments={moments} />
-      </div>
-    )
-}
-export default Timeline
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://media1.tenor.com/m/B2QvI4xBIKMAAAAC/funny-weird.gif",
+        },
+      },
+    },
+  ];
+  return (
+    <div style={{ width: "100vw", height: "100vw" }}>
+      <Chrono items={items} />
+    </div>
+  );
+};
+export default Timeline;
--- TimelineApp/src/pages/home/home.jsx
+++ TimelineApp/src/pages/home/home.jsx
@@ -16,32 +16,12 @@ function Home() {
     {{/if}} */}
     
     <button id="get-history">What else happened on this day ?</button>
+    
     <p id="historical">
     </p>
 
-    <div class="timeline-wrapper">
-      <ul class="timeline">
-        <Timeline/>
-        {/* {{#each moments as |moment| }}
-        <li data-date="{{moment.date}}">
-          <span class="title">{{moment.title}}</span>
-          <div id="moment-dots" class="data show">
-            <h3 class="description">{{moment.description}}</h3>
-            <small class="date"> {{moment.date}}</small>
-            <p class="^details">
-              {{moment.details}}
-            </p>
-            <button data-id={{moment.id}}>What else happened on this day ?</button>
-
-
-            {{!-- <span class="close">Click to close</span> --}}
-          </div>
-        </li>
-        {{/each}} */}
-    
-      </ul>
-    </div>
-      </>
+    <Timeline/>
+    </>
     )
   }
   
