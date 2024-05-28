538e31c4de7226074d8451a8f2965663ab1a70c1
Allan Elias <111256927+EliasAllan@users.noreply.github.com>
2024-05-28 18:25:19 -0400

    installed react bootstrap

--- TimelineApp/src/pages/timeline-page/Timelinereactpage.jsx
+++ TimelineApp/src/pages/timeline-page/Timelinereactpage.jsx
@@ -9,9 +9,15 @@ function TimelinePage() {
       <>
        <link rel="stylesheet" href="timeline.css"></link>
 
- 
-    
+       <h3 class="hints">
+      Click on given circles to see more info
     <button id="get-history">What else happened on this day ?</button>
+    </h3>
+
+    {/* {{#if logged_in}}
+<button onclick="window.location.href='/newmoment'" id="add-moment-btn">Add a moment in time</button>
+    {{/if}} */}
+    
     
     <p id="historical">
     </p>
