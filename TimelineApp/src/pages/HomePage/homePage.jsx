0055f79fe0e51a17bc4077c5aa613c36258fe5e9
Allan Elias <111256927+EliasAllan@users.noreply.github.com>
2024-05-31 12:18:12 -0400

    forms installed

--- TimelineApp/src/pages/HomePage/homePage.jsx
+++ TimelineApp/src/pages/HomePage/homePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+
 function HomePage (){
-    return( <> 
-    <h1>This is the homepage</h1>
-    </>
-    )
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+    
+      return (
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <input {...register('firstName')} />
+          <input {...register('lastName', { required: true })} />
+          {errors.lastName && <p>Last name is required.</p>}
+          <input {...register('age', { pattern: /\d+/ })} />
+          {errors.age && <p>Please enter number for age.</p>}
+          <input type="submit" />
+        </form>
+      );
 }
 
 export default HomePage
